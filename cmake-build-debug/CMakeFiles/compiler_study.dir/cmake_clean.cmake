dd358eab463892e2f0186a36828f6480408add0c
tom--bo <otsuka.t.2013@gmail.com>
2019-02-10 13:35:56 +0900

    update

---
+++ cmake-build-debug/CMakeFiles/compiler_study.dir/cmake_clean.cmake
@@ -0,0 +1,16 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/compiler_study.dir/flex_bison/2/fb1-5.tab.c.o"
+  "CMakeFiles/compiler_study.dir/flex_bison/2/lex.yy.c.o"
+  "CMakeFiles/compiler_study.dir/step0/test1.c.o"
+  "CMakeFiles/compiler_study.dir/step0/test3.c.o"
+  "CMakeFiles/compiler_study.dir/step1/9cc.c.o"
+  "CMakeFiles/compiler_study.dir/step2/9cc.c.o"
+  "CMakeFiles/compiler_study.dir/step3and4/9cc.c.o"
+  "compiler_study.pdb"
+  "compiler_study"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/compiler_study.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
